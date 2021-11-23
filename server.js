f59c5359a9ae5d59389483485e634002c1907850
Léa Hamadouche <leahamadouche@hotmail.com>
2021-11-23 12:03:10 +0100

    GET Partenaires

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 //Importation des modules utilisés
-const app = require("./app");
-const http = require("http");
-//const { Db } = require("mongodb"); //Pour le test de la connexion à la bdd
-require("dotenv").config();
-
-/*
-//Test de la connexion à la bdd
-const db = require("./kernel/db");
-db.connect();
-*/
+const app = require('./app');
+const http = require('http');
+require('dotenv').config();
 
 //Déclaration/Forçage du port d'écoute
 const PORT = process.env.PORT || 8080;
-app.set("port", PORT);
+app.set('port', PORT);
 
 //Création du serveur web
 var server = http.createServer(app);
