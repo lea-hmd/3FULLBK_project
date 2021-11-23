9329ac2396af4ce8334fc8ea4cf0edddc3a8afcc
Léa Hamadouche <leahamadouche@hotmail.com>
2021-11-23 15:42:29 +0100

    Refacto de la création du serveur

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 //Importation des modules utilisés
-const app = require("./app");
-const http = require("http");
-//const { Db } = require("mongodb"); //Pour le test de la connexion à la bdd
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
 
-/*
-//Test de la connexion à la bdd
-const db = require("./kernel/db");
-db.connect();
-*/
+var corsOptions = {
+  origin: 'http://localhost:8080',
+};
 
-//Déclaration/Forçage du port d'écoute
+app.use(cors(corsOptions));
+
+// Parse et 'traite' le contenu des requêtes de type application/json
+app.use(express.json());
+
+// Parse et 'traite' le contenu des requêtes de type application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//Route principale du site
+app.get('/', (req, res) => {
+  //Permet d'afficher correctement les accents
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  //Phrase affichée si le serveur est bien fonctionnel et lancé
+  res.write('Vous êtes à la racine du Projet plaquette 3FULLBK du groupe 09 !');
+  res.send();
+});
+
+//Création du serveur web et écoute des requêtes
 const PORT = process.env.PORT || 8080;
-app.set("port", PORT);
-
-//Création du serveur web
-var server = http.createServer(app);
-server.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
