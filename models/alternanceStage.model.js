5fdcfb5696aa3b5063dc57e7adca973b6bfbaee9
Léa Hamadouche <leahamadouche@hotmail.com>
2021-11-29 20:21:56 +0100

    Schéma models Financement - Contact - Alternance/Stage

---
+++ models/alternanceStage.model.js
@@ -0,0 +1,14 @@
+//Création du modèle et de son schéma de données
+module.exports = (mongoose) => {
+  const AlternanceStage = mongoose.model(
+    "alternance_stage", //Nom de la collection
+    mongoose.Schema(
+      {
+        title: { type: String, unique: true },
+        description: String,
+      },
+      { timestamps: true }
+    )
+  );
+  return AlternanceStage;
+};
