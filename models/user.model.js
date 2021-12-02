eddc9326eb8f44d01493f5e850b19cc40f09ae76
Léa Hamadouche <leahamadouche@hotmail.com>
2021-12-02 19:17:37 +0100

    Updated users/roles models

--- models/user.model.js
+++ models/user.model.js
@@ -1,25 +1,21 @@
 //Création du modèle et de son schéma de données
 module.exports = (mongoose) => {
-  const uniqueValidator = require("mongoose-unique-validator");
-  const userSchema = mongoose.Schema(
-    {
-      firstName: { type: String, required: true },
-      lastName: { type: String, required: true },
-      email: { type: String, unique: true, required: true },
-      password: { type: String, required: true },
-      role: {
-        type: Boolean,
-        default: "contentManager",
-        enum: ["contentManager", "admin"],
-      },
-      accessToken: String,
-    },
-    { timestamps: true }
-  );
   const User = mongoose.model(
     "user", //Nom de la collection
-    userSchema
+    mongoose.Schema(
+      {
+        username: { type: String, required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        roles: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "role",
+          },
+        ],
+      },
+      { timestamps: true }
+    )
   );
-  userSchema.plugin(uniqueValidator);
   return User;
 };
