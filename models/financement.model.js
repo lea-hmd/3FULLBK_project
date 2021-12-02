e4b8123d798de132ce1494921919c3aec61e606f
Léa Hamadouche <leahamadouche@hotmail.com>
2021-12-02 12:42:38 +0100

    Remove userId from content model

--- models/financement.model.js
+++ models/financement.model.js
@@ -1,12 +1,11 @@
 //Création du modèle et de son schéma de données
 module.exports = (mongoose) => {
   const Financement = mongoose.model(
-    "financement", //Nom de la collection
+    'financement', //Nom de la collection
     mongoose.Schema(
       {
         title: { type: String, unique: true, required: true },
         description: String,
-        userId: { type: String, required: true },
       },
       { timestamps: true }
     )
--- models/partenaire.model.js
+++ models/partenaire.model.js
@@ -1,14 +1,13 @@
 //Création du modèle et de son schéma de données
 module.exports = (mongoose) => {
   const Partenaire = mongoose.model(
-    "campus_entreprise", //Nom de la collection
+    'campus_entreprise', //Nom de la collection
     mongoose.Schema(
       {
         name: { type: String, unique: true, required: true },
         isCampus: { type: Boolean, required: true },
         isEntreprise: { type: Boolean, required: true },
         imageUrl: { type: String, required: true },
-        userId: { type: String, required: true },
       },
       { timestamps: true }
     )
