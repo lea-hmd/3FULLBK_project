bd8fcf8872fb300b8883b2d0580449eeec248afe
Léa Hamadouche <leahamadouche@hotmail.com>
2021-12-03 15:33:46 +0100

    Protected routes

--- routes/financements.routes.js
+++ routes/financements.routes.js
@@ -1,36 +1,57 @@
 module.exports = (app) => {
-  const financements = require("../controllers/financements.controller");
-  var router = require("express").Router();
+  const financements = require('../controllers/financements.controller');
+  const authJwt = require('../middlewares/authJwt');
+  var router = require('express').Router();
 
   /*------------ AJOUT ------------*/
 
   //Route de la requête d'ajout d'une nouvelle donnée de la page financement
-  router.post("/financements/addOne", financements.createOne);
+  router.post(
+    '/financements/addOne',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    financements.createOne
+  );
 
   //Route de la requête d'ajout de nouvelles données de la page financement
-  router.post("/financements/addMany", financements.createMany);
+  router.post(
+    '/financements/addMany',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    financements.createMany
+  );
 
   /*------------ AFFICHAGE ------------*/
 
   //Route de la requête d'affichage de toutes les données de la page financement
-  router.get("/financements/all", financements.getAll);
+  router.get('/financements/all', financements.getAll);
 
   //Route de la requête d'affichage des données de la page financement en fonction de leur titre
-  router.get("/financements/:title", financements.getByTitle);
+  router.get('/financements/:title', financements.getByTitle);
 
   /*------------ MODIFICATION ------------*/
 
   //Route de la requête de modification d'une donnée de la page financement en fonction de son id
-  router.put("/financements/updateById/:id", financements.updateById);
+  router.put(
+    '/financements/updateById/:id',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    financements.updateById
+  );
 
   /*------------ SUPPRESSION ------------*/
 
   //Route de la requête de suppression de toutes les données de la page financement
-  router.delete("/financements/deleteAll", financements.deleteAll);
+  router.delete(
+    '/financements/deleteAll',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    financements.deleteAll
+  );
 
   //Route de la requête de suppression d'une donnée de la page financement en fonction de son id
-  router.delete("/financements/deleteById/:id", financements.deleteById);
+  router.delete(
+    '/financements/deleteById/:id',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    financements.deleteById
+  );
 
   //Route racine de l'api
-  app.use("/api", router);
+  app.use('/api', router);
 };
--- routes/partenaires.routes.js
+++ routes/partenaires.routes.js
@@ -1,45 +1,74 @@
 module.exports = (app) => {
-  const partenaires = require("../controllers/partenaires.controller");
-  var router = require("express").Router();
+  const partenaires = require('../controllers/partenaires.controller');
+  const authJwt = require('../middlewares/authJwt');
+  var router = require('express').Router();
 
   /*------------ AJOUT ------------*/
 
   //Route de la requête de création d'un nouveau partenaire
-  router.post("/partenaires/addOne", partenaires.createOne);
+  router.post(
+    '/partenaires/addOne',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.createOne
+  );
 
   //Route de la requête de création de plusieurs partenaires
-  router.post("/partenaires/addMany", partenaires.createMany);
+  router.post(
+    '/partenaires/addMany',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.createMany
+  );
 
   /*------------ AFFICHAGE ------------*/
 
   //Route de la requête d'affichage de tous les partenaires
-  router.get("/partenaires/all", partenaires.getAll);
+  router.get('/partenaires/all', partenaires.getAll);
 
   //Route de la requête d'affichage de tous les campus partenaires
-  router.get("/partenaires/campus", partenaires.getCampus);
+  router.get('/partenaires/campus', partenaires.getCampus);
 
   //Route de la requête d'affichage de toutes les entreprises partenaires
-  router.get("/partenaires/entreprises", partenaires.getEntreprises);
+  router.get('/partenaires/entreprises', partenaires.getEntreprises);
 
   /*------------ MODIFICATION ------------*/
 
   //Route de la requête de modification d'un partenaire via son id
-  router.put("/partenaires/updateById/:id", partenaires.updateById);
+  router.put(
+    '/partenaires/updateById/:id',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.updateById
+  );
 
   //Route de la requête de modification d'un partenaire via son nom
-  router.put("/partenaires/updateByName/:name", partenaires.updateByName);
+  router.put(
+    '/partenaires/updateByName/:name',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.updateByName
+  );
 
   /*------------ SUPPRESSION ------------*/
 
   //Route de la requête de suppression de tous les partenaires
-  router.delete("/partenaires/deleteAll", partenaires.deleteAll);
+  router.delete(
+    '/partenaires/deleteAll',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.deleteAll
+  );
 
   //Route de la requête de suppression d'un partenaire en fonction de son nom
-  router.delete("/partenaires/deleteByName/:name", partenaires.deleteByName);
+  router.delete(
+    '/partenaires/deleteByName/:name',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.deleteByName
+  );
 
   //Route de la requête de suppression d'un partenaire en fonction de son id
-  router.delete("/partenaires/deleteById/:id", partenaires.deleteById);
+  router.delete(
+    '/partenaires/deleteById/:id',
+    [authJwt.verifyToken, authJwt.isAdmin, authJwt.isContentManager],
+    partenaires.deleteById
+  );
 
   //Route racine de l'api
-  app.use("/api", router);
+  app.use('/api', router);
 };
