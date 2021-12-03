bdbe1d5249082e872077e1db3d680ffc57b1c340
Léa Hamadouche <leahamadouche@hotmail.com>
2021-12-03 15:45:14 +0100

    Protected routes auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,15 @@
 module.exports = function (app) {
   //Importation des modules utilisés
-  const checking = require("../middlewares/verifyRoleSignUp");
-  const auth = require("../controllers/auth.controller");
-  var router = require("express").Router();
+  const checking = require('../middlewares/verifyRoleSignUp');
+  const auth = require('../controllers/auth.controller');
+  const authJwt = require('../middlewares/authJwt');
+  var router = require('express').Router();
 
   //Spécification des headers
   app.use(function (req, res, next) {
     res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
     );
     next();
   });
@@ -16,11 +17,16 @@ module.exports = function (app) {
   /*------------ AJOUT ------------*/
 
   //Route de la requête de création d'un utilisateur
-  router.post("/auth/signup", checking.verifyRoleSignUp, auth.signup);
+  router.post(
+    '/auth/signup',
+    [authJwt.verifyToken, authJwt.isAdmin],
+    checking.verifyRoleSignUp,
+    auth.signup
+  );
 
   //Route de la requête dde connexion d'un utilisateur
-  router.post("/auth/signin", auth.signin);
+  router.post('/auth/signin', auth.signin);
 
   //Route racine de l'api
-  app.use("/api", router);
+  app.use('/api', router);
 };
