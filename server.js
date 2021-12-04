f6357bb1277e43302c77a4a0b2f3f5382a4e131e
Léa Hamadouche <leahamadouche@hotmail.com>
2021-12-04 12:47:28 +0100

    Added contactEntreprises routes into server.js

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ app.get("/", (req, res) => {
 require("./routes/partenaires.routes")(app);
 require("./routes/certificationsPro.routes")(app);
 require("./routes/financements.routes")(app);
+require("./routes/contactEntreprises.routes")(app);
 require("./routes/users.routes")(app);
 require("./routes/auth.routes")(app);
 
